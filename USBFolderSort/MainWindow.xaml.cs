11028b0c6709b727d5f1664b29cbd965ab5d7c3d
DmasterK <daniel.kaltenbrunner@gmx.at>
2017-01-11 21:38:53 +0100

    update

--- USBFolderSort/MainWindow.xaml.cs
+++ USBFolderSort/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace USBFolderSort
         public MainWindow()
         {
             InitializeComponent();
+            loadRemovableDrives();
+        }
+
+        private void btn_refresh_devices_Click(object sender, RoutedEventArgs e)
+        {
+            btn_refresh_devices.IsEnabled = false;
+            loadRemovableDrives();
+            btn_refresh_devices.IsEnabled = true;
+        }
+
+        private void loadRemovableDrives()
+        {
+            emptyDrivesList();
+            var allDrives = DriveInfo.GetDrives().Where(w=>w.DriveType.Equals(DriveType.Removable) && w.IsReady == true);
+            foreach (var d in allDrives)
+            {
+                lv_all_drives.Items.Add(d.ToString());
+            }
+        }
+
+        private void emptyDrivesList()
+        {
+            var cnt = lv_all_drives.Items.Count;
+            for (int i = 0; i < cnt; i++)
+            {
+                lv_all_drives.Items.RemoveAt(i);
+            }
+        }
+
+        private void btn_sort_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
